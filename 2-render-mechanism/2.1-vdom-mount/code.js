71fbb0f1ce6abfcaae05e074d4bcf47b39a09a1a
Matthew Ailes <mattailes@gmail.com>
2019-03-25 14:06:31 -0400

    render mechanism - part 1

--- 2-render-mechanism/2.1-vdom-mount/code.js
+++ 2-render-mechanism/2.1-vdom-mount/code.js
@@ -1,7 +1,42 @@
 export function h(tag, attrs, children) {
-
+  return {
+    tag,
+    attrs,
+    children
+  }
 }
 
 export function mount(vdom, container) {
+  let dom
+  switch(typeof vdom) {
+    case 'string':
+      dom = mountText(vdom)
+      break
+    default:
+      dom = mountElement(vdom)
+  }
 
+  container.appendChild(dom)
+}
+
+function mountElement(vdom) {
+  const dom = document.createElement(vdom.tag)
+
+  if (vdom.attrs) {
+    Object.keys(vdom.attrs).forEach(key => {
+      dom.setAttribute(key, vdom.attrs[key])
+    })
+  }
+
+  if (vdom.children) {
+    vdom.children.forEach(child => {
+      mount(child, dom)
+    })
+  }
+
+  return dom
+}
+
+function mountText(vdom) {
+  return document.createTextNode(vdom)
 }
