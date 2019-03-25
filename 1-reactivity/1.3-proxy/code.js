d03957474b0aa397f998813c30d1ab67b7f21a9d
Matthew Ailes <mattailes@gmail.com>
2019-03-25 11:36:59 -0400

    part 3 - reactivity and proxies

--- 1-reactivity/1.3-proxy/code.js
+++ 1-reactivity/1.3-proxy/code.js
@@ -2,7 +2,39 @@ import { Dep, effect } from '../1.2-getter-setter/code.js'
 
 export { effect }
 
-// ---
-export function observable(obj) {
+const handlers = {
+  get(target, key) {
+    const dep = getDep(target, key)
+    dep.depend()
 
+    return target[key]
+  },
+
+  set(target, key, value) {
+    target[key] = value
+    const dep = getDep(target, key)
+    dep.notify()
+    
+    return true
+  }
+}
+
+const depsMap = new WeakMap()
+
+function getDep(target, key) {
+  if (!depsMap.has(target)) {
+    depsMap.set(target, {})
+  }
+
+  const deps = depsMap.get(target)
+
+  if (!deps[key]) {
+    deps[key] = new Dep()
+  }
+
+  return deps[key]
+}
+
+export function observable(obj) {
+  return new Proxy(obj, handlers)
 }
