6b126f8c1820862ce4731337669e8f24c7cf0c7e
Matthew Ailes <mattailes@gmail.com>
2019-03-25 10:32:35 -0400

    part 1 - reactivity

--- 1-reactivity/1.1-dependency-tracking/code.js
+++ 1-reactivity/1.1-dependency-tracking/code.js
@@ -1,7 +1,26 @@
-export class Observable {
+let currentRunner
 
+export class Observable {
+  constructor(count) {
+    this.count = count
+    this.subscribers = new Set()
+  }
+
+  get() {
+    if (currentRunner) {
+      this.subscribers.add(currentRunner)
+    }
+    return this.count
+  }
+
+  set(count) {
+    this.count = count
+    this.subscribers.forEach(sub => sub())
+  }
 }
 
 export function effect(runner) {
-
+  currentRunner = runner
+  runner()
+  currentRunner = null
 }
