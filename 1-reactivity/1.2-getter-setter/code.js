53244d7d9dbe7ebda12e4bc25bed6653d54aae1b
Matthew Ailes <mattailes@gmail.com>
2019-03-25 11:05:45 -0400

    part 2 - reactive getters/settes

--- 1-reactivity/1.2-getter-setter/code.js
+++ 1-reactivity/1.2-getter-setter/code.js
@@ -1,11 +1,43 @@
 export class Dep {
+  constructor(value) {
+    this.subscribers = new Set()
+  }
 
+  depend() {
+    if (currentRunner) {
+      this.subscribers.add(currentRunner)
+    }
+  }
+
+  notify() {
+    this.subscribers.forEach(sub => sub())
+  }
 }
 
+let currentRunner
 export function effect(runner) {
-
+  currentRunner = runner
+  runner()
+  currentRunner = null
 }
 
 export function observable(obj) {
+  Object.keys(obj).forEach(key => {
+    let realValue = obj[key]
+    const dep = new Dep()
 
+    Object.defineProperty(obj, key, {
+      get() {
+        dep.depend()
+        return realValue
+      },
+
+      set(newValue) {
+        realValue = newValue
+        dep.notify()
+      }
+    })
+  })
+
+  return obj
 }
